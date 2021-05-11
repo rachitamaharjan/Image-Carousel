6f7ccca0aa0935db258b33b6de57053bd5d4d6ec
smilerac <smilerac15@gmail.com>
2021-05-11 10:57:14 +0545

    fixed some errors

--- js/utils.js
+++ js/utils.js
@@ -4,11 +4,15 @@ var imgVar = document.getElementsByTagName('img')
 var indexNext = 0
 var indexPrev = imgVar.length
 
+imgWrapper[0].style.marginLeft = '0px'
 
 function changeIndex(val){
     console.log('val',val)
     if (val == 'next'){
         ++indexNext
+        if (indexNext > imgVar.length){
+            indexNext = 1
+        }
         console.log('indexNext',indexNext)
         return indexNext
     }
@@ -38,6 +42,52 @@ for(i=0; i<imgVar.length; i++){
     var indicator = document.createElement('button');
     indicator.className = "indicator";
     indicatorContainer.appendChild(indicator);
+}
+
+
+
+nextSlide.onclick = function() {
+    // imgWrapper[0].width = imgVar[0].width*imgVar.length
+    var index = changeIndex('next')
+    console.log('index',index)
+    increment = (imgVar[index % imgVar.length].width) * (index - 1)
+    console.log('increment',increment)
+    console.log('index % imgVar.length',index % imgVar.length)
+
+    if (index > index % imgVar.length){
+        imgWrapper[0].style.marginLeft = '0px'
+    }
+    setInterval(function() {
+        if (increment < ((imgVar[index % imgVar.length].width) * (index % imgVar.length))){
+            increment += 25;
+            imgWrapper[0].style.marginLeft = '-' + increment + 'px'
+            // console.log('position',imgWrapper[0].style.marginLeft)
+        }
+    }, 25);
+}
+
+previousSlide.onclick = function() {
+
+    
+    console.log('o',imgWrapper[0].style.marginLeft)
+    console.log('next',document.getElementById('next').onclick)
+    increment = 0
+    console.log('imgvar width',imgVar[0].width)
+    console.log('changeindex',changeIndex('prev'))
+    var index = changeIndex('prev')
+    // if (imgWrapper[0].style.marginLeft == '0px'){
+    //     imgWrapper[0].style.marginLeft = '-' + (imgVar[index].width * imgVar.length)+'px'
+    //     console.log('imgwrapper1',imgWrapper[0].style.marginLeft)
+    //     console.log('1',imgVar[index].width * imgVar.length)
+    //     console.log('2',imgVar.length)
+    // }
+    setInterval(function() {
+        if (increment<imgVar[index].width){
+            increment = increment + 25;
+            imgWrapper[0].style.marginLeft =  increment + 'px'
+            // console.log(imgWrapper[0].style.marginLeft)
+        }
+    }, 25);
 }
 
 
